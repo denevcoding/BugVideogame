df20ed56ba3bef33160812858558b42e80c0ed49
BlueEyesFTWuao <jose.bueno@uao.edu.co>
2023-05-19 21:12:06 -0500

    Update playerhealth checkpoint

--- Bug UAO Videogame/Assets/JOSE/Checkpoint.cs
+++ Bug UAO Videogame/Assets/JOSE/Checkpoint.cs
@@ -3,16 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{    
+    [SerializeField]GameObject player;
+    public Transform lastCheckPoint;
+    [SerializeField] Vector3 vectorPoint;
 
+    [SerializeField]  float dead;
     // Update is called once per frame
     void Update()
     {
-        
+        CheckHeightDead();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Checkpoint")
+        {
+            lastCheckPoint = other.transform;
+            vectorPoint = other.transform.position;
+            Debug.Log("Entro El checkpoint");
+        }
+    }
+
+    public void CheckHeightDead()
+    {
+        if (player.transform.position.y < -dead)
+        {
+            RespawnOnCheckPoint(); 
+
+        }
+    }
+
+    public void RespawnOnCheckPoint()
+    {
+        player.GetComponent<CharacterController>().enabled = false;
+        player.transform.position = vectorPoint;
+        player.GetComponent<CharacterController>().enabled = true;
     }
 }
--- Bug UAO Videogame/Assets/JOSE/PlayerHealth.cs
+++ Bug UAO Videogame/Assets/JOSE/PlayerHealth.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // This function is NOT built in to Unity
-    // It will only be called manually by our own code
-    // It must be marked "public" so our other scripts can access it
+    Checkpoint checkpoint;
+
+    private void Start()
+    {
+        checkpoint = FindObjectOfType<Checkpoint>();
+    }
+
     public void Kill()
     {
-        // This will destroy the gameObject that this script is attached to
-        Destroy(gameObject);
+        checkpoint.RespawnOnCheckPoint();
     }
 }
